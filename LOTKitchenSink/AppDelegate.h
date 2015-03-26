89acc3723ee8670da5e51f02254079f8a9ebcdec
toturgul <toturgul@gmail.com>
2015-03-26 16:06:08 -0400

    Initial Commit

---
+++ LOTKitchenSink/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LOTKitchenSink
+//
+//  Created by Levan Toturgul on 3/26/15.
+//  Copyright (c) 2015 Levan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LOTKitchenSink/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LOTKitchenSink
+//
+//  Created by Levan Toturgul on 3/26/15.
+//  Copyright (c) 2015 Levan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
